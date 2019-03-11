5a362031f02ea957914d2cf1e4dc8918bce38bdb
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-03-11 18:17:51 +0530

    added socialite scopes

--- app/Http/Controllers/Api/V1/SocialController.php
+++ app/Http/Controllers/Api/V1/SocialController.php
@@ -43,8 +43,8 @@ class SocialController extends Controller
 
     public function registerViaSocialNetwork($data)
     {
-        dd($data);
-        
+        dd($data['avatar']);
+
         $user = $this->user->register([
             'name' => $data['name'],
             'email' => $data['email'],
