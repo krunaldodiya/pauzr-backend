173efa9ba76593778e91031e6f574e4f462e6385
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-03-11 10:27:30 +0530

    added google facebook auth socialite

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        @yield('title')
+        <title>
+            @yield('title')
+        </title>
     </head>
     
     <body>
--- resources/views/response.blade.php
+++ resources/views/response.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.master')
 
-@section('title', 'Page Title')
-    <title>{{json_encode($user)}}</title>
-@endsection
+@section('title', {{json_encode($user)}})
 
 @section('content')
-<div class="container">
-    {{ $user->name }}
-</div>
+    <div class="container">
+        {{ $user->name }}
+    </div>
 @endsection
