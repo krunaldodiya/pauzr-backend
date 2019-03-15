54cd0306117d2a44e2018841d0337bc582186453
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-03-15 13:18:34 +0530

    access to telescope granted for krunal

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,14 +21,16 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->isLocal()) {
-                return true;
-            }
+            return true;
 
-            return $entry->isReportableException() ||
-                $entry->isFailedJob() ||
-                $entry->isScheduledTask() ||
-                $entry->hasMonitoredTag();
+            // if ($this->app->isLocal()) {
+            //     return true;
+            // }
+
+            // return $entry->isReportableException() ||
+            //     $entry->isFailedJob() ||
+            //     $entry->isScheduledTask() ||
+            //     $entry->hasMonitoredTag();
         });
     }
 
