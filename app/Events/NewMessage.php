0b3f0b718f78ca1b4492bbbcf1e3e9e637ed5f7c
krunal dodiya <kunal.dodiya1@gmail.com>
2019-04-05 10:52:41 +0530

    added laravel websockets

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -33,6 +33,6 @@ class NewMessage implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new PrivateChannel('home');
     }
 }
