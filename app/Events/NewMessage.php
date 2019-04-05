7090ad396d8e69f6f3d0485cf66aaacc0818bc4c
krunal dodiya <kunal.dodiya1@gmail.com>
2019-04-05 10:34:41 +0530

    added laravel websockets

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -10,18 +10,20 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class NewMessage
+class NewMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($mesage)
     {
-        //
+        $this->message = $mesage;
     }
 
     /**
