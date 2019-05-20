0bf28abdbc6ccfeb3c65230aca6edad72224793d
krunal dodiya <kunal.dodiya1@gmail.com>
2019-05-20 13:36:04 +0530

    init

--- app/Http/Controllers/TimerController.php
+++ app/Http/Controllers/TimerController.php
@@ -67,14 +67,6 @@ class TimerController extends Controller
                     'duration' => $user->timer_history->sum('duration')
                 ];
             })
-            // ->sortByDesc('duration')
-            // ->map(function ($data, $index) {
-            //     return [
-            //         'user' => $data['user'],
-            //         'duration' => $data['duration'],
-            //         'rank' => $index + 1
-            //     ];
-            // })
             ->toArray();
 
         return compact('minutes_saved', 'points_earned', 'rankings');
