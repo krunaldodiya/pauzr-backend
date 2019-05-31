595958692fd02fa960dfd672b3b63ca8aaaa914a
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-05-31 09:01:15 +0530

    started flutter provider pattern

--- config/backup.php
+++ config/backup.php
@@ -120,7 +120,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => env('MAIL_USERNAME'),
         ],
 
         'slack' => [
