5407ac1134366c199eff2af3a2c1d55178a3f4c3
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-05-31 08:23:42 +0530

    started flutter provider pattern

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,14 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('coupomated:add-stores')
             ->hourly();
+
+        $schedule->command('backup:clean')
+            ->daily()
+            ->at('01:00');
+
+        $schedule->command('backup:run')
+            ->daily()
+            ->at('02:00');
     }
 
     /**
