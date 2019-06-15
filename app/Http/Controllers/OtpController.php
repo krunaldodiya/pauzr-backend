be52e29fbaff21142f0b86d57d74d9e163802cfa
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-15 11:08:57 +0530

    started flutter provider pattern

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -70,7 +70,7 @@ class OtpController extends Controller
                 throw new OtpVerificationFailed($verifyOtp->message);
             }
         } catch (Exception $e) {
-            return response(['error' => $e->getMessage()], 400);
+            return response(['error' => ['otp' => $e->getMessage()]], 400);
         }
     }
 }
