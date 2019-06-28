eaa0316b10fe4688430b5141e231ea8449b5cb75
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-28 08:45:29 +0530

    init

--- app/Http/Requests/UpdateUser.php
+++ app/Http/Requests/UpdateUser.php
@@ -17,7 +17,7 @@ class UpdateUser extends FormRequest
         return [
             'id' => 'required',
             'name' => 'required|min:3',
-            'email' => 'required|email|unique:users|sometimes',
+            'email' => 'required|email|unique:users,' . $this->id,
             'dob' => 'required|min:3',
             'gender' => 'required',
             'city_id' => 'required',
