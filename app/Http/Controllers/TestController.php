d50b939d3e3f9a25de6088fe095368db73a1ad32
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-04 21:36:40 +0530

    added date to timer

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 use App\Repositories\TimerRepository;
-use Carbon\Carbon;
-use App\User;
-use App\Timer;
 
 class TestController extends Controller
 {
@@ -22,32 +19,6 @@ class TestController extends Controller
 
     public function check(Request $request)
     {
-        $user = User::find(2);
-
-        $duration = 'Week';
-        $period = $duration == 'Week' ? Carbon::now()->startOfWeek() : Carbon::now()->startOfMonth();
-
-        $timers = Timer::whereHas('city', function ($query) use ($user) {
-            return $query->where('country_id', $user->country_id);
-        })
-            ->where('created_at', '>=', $period)
-            ->orderBy('duration', 'desc')
-            ->get()
-            ->groupBy('user_id');
-
-        $data = [];
-
-        foreach ($timers as $user_id => $timer) {
-            $data[] = [
-                'country_id' => $user->country_id,
-                'user_id' => $user_id,
-                'minutes' => $timer->sum('duration'),
-                'duration' => $duration,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ];
-        }
-
-        return $data;
+        return 'test';
     }
 }
