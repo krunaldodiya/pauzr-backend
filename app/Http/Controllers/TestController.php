c986334eaa71d5ff2d9e919385ab494a519c911d
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-18 17:15:45 +0530

    init

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 use App\Repositories\TimerRepository;
-use App\User;
-use Intervention\Image\Facades\Image;
-use Illuminate\Support\Facades\Storage;
+use JD\Cloudder\Facades\Cloudder;
 
 class TestController extends Controller
 {
@@ -22,33 +20,12 @@ class TestController extends Controller
 
     public function check(Request $request)
     {
-        $API_KEY = "5dgRQnLfbglTcwXrmobBceaeE64=";
-        $SECRET_KEY = "5dgRQnLfbglTcwXrmobBceaeE64=";
-        $IMAGEKIT_ID = "pauzrapp";
+        $filename = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201907/super-30_Hrithik_0-770x433.jpeg?nCjjGiayMRUlj08VebELl_d7Ks7XWxQr";
 
-        $UPLOAD_API_ENDPOINT = "https://upload.imagekit.io/rest/api/image/v2/$IMAGEKIT_ID";
+        Cloudder::upload($filename, null);
 
-        $FILENAME = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201907/super-30_Hrithik_0-770x433.jpeg?nCjjGiayMRUlj08VebELl_d7Ks7XWxQr";
-        $file = base64_encode(file_get_contents($FILENAME));
+        $data = Cloudder::getResult();
 
-        $TIMESTAMP = strval(time());
-        $param_string = "apiKey=$API_KEY&filename=$FILENAME&timestamp=$TIMESTAMP";
-        $signature = hash_hmac("sha1", $param_string, $SECRET_KEY);
-
-        $fields = [
-            "signature" => $signature,
-            "filename" => $FILENAME,
-            "timestamp" => $TIMESTAMP,
-            "useUniqueFilename" => true,
-            "apiKey" => $API_KEY,
-            "file" => $file
-        ];
-
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request("POST", $UPLOAD_API_ENDPOINT, [
-            'query' => $fields
-        ]);
-
-        return json_decode($response->getBody());
+        return ['secure_url' => $data['secure_url']];
     }
 }
