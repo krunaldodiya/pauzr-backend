49cdac8fc8097e37446238305e2f43d371d8fad6
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-18 16:39:39 +0530

    init

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 use App\Repositories\TimerRepository;
 use App\User;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class TestController extends Controller
 {
@@ -20,8 +22,33 @@ class TestController extends Controller
 
     public function check(Request $request)
     {
-        $user = User::find(1);
+        $API_KEY = "5dgRQnLfbglTcwXrmobBceaeE64=";
+        $SECRET_KEY = "5dgRQnLfbglTcwXrmobBceaeE64=";
+        $IMAGEKIT_ID = "pauzrapp";
 
-        $this->timerRepository->calculateWinners($user, 'Week');
+        $UPLOAD_API_ENDPOINT = "https://upload.imagekit.io/rest/api/image/v2/$IMAGEKIT_ID";
+
+        $FILENAME = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201907/super-30_Hrithik_0-770x433.jpeg?nCjjGiayMRUlj08VebELl_d7Ks7XWxQr";
+        $file = base64_encode(file_get_contents($FILENAME));
+
+        $TIMESTAMP = strval(time());
+        $param_string = "apiKey=$API_KEY&filename=$FILENAME&timestamp=$TIMESTAMP";
+        $signature = hash_hmac("sha1", $param_string, $SECRET_KEY);
+
+        $fields = [
+            "signature" => $signature,
+            "filename" => $FILENAME,
+            "timestamp" => $TIMESTAMP,
+            "useUniqueFilename" => true,
+            "apiKey" => $API_KEY,
+            "file" => $file
+        ];
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request("POST", $UPLOAD_API_ENDPOINT, [
+            'query' => $fields
+        ]);
+
+        return json_decode($response->getBody());
     }
 }
