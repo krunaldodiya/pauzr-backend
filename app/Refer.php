15bd6e38d675b1fc297a7e9ffc7e46464a8a5215
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-29 12:48:37 +0530

    init

--- app/Refer.php
+++ app/Refer.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Refer extends Model
 {
-    //
+    protected $fillable = [
+        'ip_address'
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
 }
