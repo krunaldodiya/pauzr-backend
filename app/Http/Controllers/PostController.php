cb1dd6c6558924f4d4b094ffaa0daaa3cff26eb2
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-31 21:01:48 +0530

    init

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
     {
         Image::where(['id' => $request->postId])
             ->update([
-                'description' => $request->description ?? null,
+                'description' => $request->description,
                 'url' => $request->photo,
             ]);
 
