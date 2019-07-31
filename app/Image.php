c152d5fe402bd9b9c22467b4825eff1d5fc070e3
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-31 20:52:14 +0530

    init

--- app/Image.php
+++ app/Image.php
@@ -10,6 +10,11 @@ class Image extends Model
 
     protected $dates = ['created_at', 'updated_at'];
 
+    public function getCreatedAtAttribute($created_at)
+    {
+        return $created_at->diffForHumans();
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
