72eb929d88cd7b33c7ffe2f6b8bea5a5b3d440d9
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-31 09:22:55 +0530

    init

--- app/Follow.php
+++ app/Follow.php
@@ -9,4 +9,9 @@ class Follow extends Model
     protected $fillable = ['follower_id', 'following_id'];
 
     protected $dates = ['created_at', 'updated_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -9,7 +9,7 @@ class UserRepository implements UserRepositoryInterface
 {
     public function getUserById($user_id)
     {
-        return User::with('city.state.country', 'state', 'country', 'level', 'wallet', 'followers', 'following')
+        return User::with('city.state.country', 'state', 'country', 'level', 'wallet', 'followers.user', 'following.user')
             ->where(['id' => $user_id])
             ->first();
     }
