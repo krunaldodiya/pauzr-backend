cd45e6517a5852d3a7b052a1b2935a8fb10d1f65
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-31 09:00:46 +0530

    init

--- app/User.php
+++ app/User.php
@@ -74,12 +74,12 @@ class User extends Authenticatable implements JWTSubject
 
     public function followers()
     {
-        return $this->hasMany(Follow::class, 'follower_id');
+        return $this->hasMany(Follow::class, 'following_id');
     }
 
     public function following()
     {
-        return $this->hasMany(Follow::class, 'following_id');
+        return $this->hasMany(Follow::class, 'follower_id');
     }
 
     public function images()
