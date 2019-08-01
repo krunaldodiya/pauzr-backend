9bb2f2d6d285c7b97e5e599cb80929694fca96ff
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-01 17:15:58 +0530

    init

--- app/PostEarning.php
+++ app/PostEarning.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostEarning extends Model
 {
-    protected $fillable = ['user_id', 'duration', 'minutes', 'country_id'];
+    protected $fillable = ['user_id', 'post_id', 'points', 'redeemed'];
 
     protected $dates = ['created_at', 'updated_at'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
     }
 }
