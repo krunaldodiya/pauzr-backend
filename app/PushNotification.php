aa543cbc882fef6427d62e003ad69897ef4d4c2e
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-01 19:32:55 +0530

    init

---
+++ app/PushNotification.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PushNotification extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'image'
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
+}
---
+++ database/migrations/2019_08_01_192754_create_push_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePushNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('push_notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('push_notifications');
+    }
+}
