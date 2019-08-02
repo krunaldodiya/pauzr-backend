ad0ca65a775a19e1938b2e6927be20090bd698a6
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-02 15:46:13 +0530

    init

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -25,21 +25,6 @@ class TestController extends Controller
     {
         $user = User::where(['id' => 1])->first();
 
-        $post = Post::with('owner', 'likes.user.city', 'earnings')
-            ->where(['id' => 1])
-            ->first();
-
-        $post->owner->notifications()
-            ->where('data->user_id', 1)
-            ->where('data->post_id', 1)
-            ->delete();
-
-        // $toggle = $user->favorites()->toggle($post);
-
-        // if ($toggle['attached']) {
-        //     Notification::send($post->owner, new PostLiked($user->toArray(), $post->toArray()));
-        // }
-
-        return 'done';
+        return $user;
     }
 }
