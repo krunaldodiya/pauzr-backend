aa3be8fb0a7200374569c3d20c73066b42a8f29a
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-03 16:52:30 +0530

    init

--- config/database.php
+++ config/database.php
@@ -57,6 +57,13 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+                'excludeTables' => [
+                    'telescope_entries',
+                    'telescope_entries_tags',
+                    'telescope_monitoring',
+                ]
+            ],
         ],
 
         'pgsql' => [
