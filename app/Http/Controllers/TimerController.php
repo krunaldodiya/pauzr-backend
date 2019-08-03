39aeb36f1553eb6647f5665621f10e4ea2ec3309
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-03 10:18:10 +0530

    init

--- app/Http/Controllers/TimerController.php
+++ app/Http/Controllers/TimerController.php
@@ -32,6 +32,9 @@ class TimerController extends Controller
         $last_timer = Timer::where(['user_id' => $user->id])->orderBy('created_at', 'desc')->first();
         $time_passed_seconds = $last_timer->created_at->diffInSeconds(Carbon::now());
 
+        $user = $this->timerRepository->setTimer($user, $duration);
+        return ['user' => $this->userRepository->getUserById($user->id)];
+
         if ($time_passed_seconds > $duration * 60) {
             $user = $this->timerRepository->setTimer($user, $duration);
             return ['user' => $this->userRepository->getUserById($user->id)];
