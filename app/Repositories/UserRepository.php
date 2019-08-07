0e53b5143f715584b84b705c80c7c40e73ff0a22
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-08-07 10:25:26 +0530

    init

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -24,10 +24,17 @@ class UserRepository implements UserRepositoryInterface
             },
             'followers.follower_user',
             'followings.following_user',
+            'posts' => function ($query) {
+                return $query->paginate(300);
+            },
+            'posts.likes' => function ($query) {
+                return $query->paginate(500);
+            },
+            'posts.likes.user'
         ])
-            ->with('posts')->paginate(300)
-            ->with('posts.likes')->paginate(500)
-            ->with('posts.likes.user')
+            ->with('paginated_posts')->paginate(300)
+            ->with('paginated_posts.likes')->paginate(500)
+            ->with('paginated_posts.likes.user')
             ->where(['id' => $user_id])
             ->first();
     }
