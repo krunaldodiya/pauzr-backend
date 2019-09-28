411d77c82e6c1d823590108ab992439108a1a2b5
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-28 20:09:27 +0530

    lottery issue debug

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,72 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Lottery;
-use Illuminate\Http\Request;
-use App\Repositories\UserRepository;
-use App\Repositories\TimerRepository;
 use App\User;
-use App\Timer;
-use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public $userRepository;
-    public $timerRepository;
-
-    public function __construct(UserRepository $userRepository, TimerRepository $timerRepository)
-    {
-        $this->userRepository = $userRepository;
-        $this->timerRepository = $timerRepository;
-    }
-
     public function check(Request $request)
     {
-        $user = User::find(1);
+        $user = User::first();
 
         dd($user);
-
-        // $getLotterySlope = $this->getLotterySlope($user);
-
-        // dd($getLotterySlope);
-
-
-        // $lotteries = $getLotterySlope['lotteries'];
-        // $max_redeem = $getLotterySlope['max_redeem'];
-
-        // $earnings = Lottery::with('user.city')
-        //     ->where('type', 'credited')
-        //     ->where(['user_id' => $user->id])
-        //     ->where('created_at', '>=', Carbon::now()->startOfMonth())
-        //     ->get()
-        //     ->sum('amount');
-    }
-
-    private function getLotterySlope($user)
-    {
-        $lottery = config('lottery');
-
-        $minutes = Timer::where(['user_id' => $user->id])
-            ->where('created_at', '>=', Carbon::now()->startOfMonth())
-            ->get()
-            ->sum('duration');
-
-        $slopes = [
-            ["id" => 1, "min" => 0, "max" => 200],
-            ["id" => 2, "min" => 201, "max" => 1000],
-            ["id" => 3, "min" => 1001, "max" => 2000],
-            ["id" => 4, "min" => 2001, "max" => 5000],
-            ["id" => 5, "min" => 5001, "max" => 20000],
-        ];
-
-        $current = null;
-
-        foreach ($slopes as $slope) {
-            if (($minutes >= $slope['min']) && ($minutes <= $slope['max'])) {
-                $current = $lottery[$slope['id']];
-            }
-        }
-
-        return $current;
     }
 }
