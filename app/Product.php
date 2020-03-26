8176df94b5ecc26cc531da6c12d4f9f318a96d02
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-03-26 19:28:56 +0530

    added storeg

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Nova\Actions\Actionable;
 
 class Product extends Model
 {
-    //
+    use Actionable;
+
+    protected $guarded = [];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
